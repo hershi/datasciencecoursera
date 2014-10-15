40dea2cd1ff9170629642f46168fd58e4834f4bf
unknown <guyhe@HERSHI-LAP.redmond.corp.microsoft.com>
2014-10-15 01:09:40 -0700

    Committer: unknown <guyhe@HERSHI-LAP.redmond.corp.microsoft.com>
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	new file:   complete.R
    	new file:   corr.R
    	new file:   pollutantmean.R
    
     Assignment 1 solutions in R Programming course

---
+++ RProgramming/Assignment1/corr.R
@@ -0,0 +1,29 @@
+corr <- function(directory, threshold = 0) {
+    ## 'directory' is a character vector of length 1 indicating
+    ## the location of the CSV files
+
+    ## 'threshold' is a numeric vector of length 1 indicating the
+    ## number of completely observed observations (on all
+    ## variables) required to compute the correlation between
+    ## nitrate and sulfate; the default is 0
+
+    ## Return a numeric vector of correlations
+
+    results<-c()
+
+    
+    for (filename in list.files(directory)) 
+    {
+        file<-file.path(directory,filename)
+
+        csvdata<-read.csv(file)
+        completecases<-complete.cases(csvdata)
+
+        if(sum(completecases) >= threshold)
+        {
+            results<-c(results,cor(csvdata[completecases,]["nitrate"][,1], csvdata[completecases,]["sulfate"][,1]))
+        }
+    }
+
+    as.numeric(results)
+}
---
+++ RProgramming/Assignment1/pollutantmean.R
@@ -0,0 +1,26 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+    ## 'directory' is a character vector of length 1 indicating
+    ## the location of the CSV files
+
+    ## 'pollutant' is a character vector of length 1 indicating
+    ## the name of the pollutant for which we will calculate the
+    ## mean; either "sulfate" or "nitrate".
+
+    ## 'id' is an integer vector indicating the monitor ID numbers
+    ## to be used
+
+    ## Return the mean of the pollutant across all monitors list
+    ## in the 'id' vector (ignoring NA values)
+        
+    data<-c()
+    for (i in id) 
+    {
+        filename<-sprintf("%03d.csv",i)
+        file<-file.path(directory,filename)
+
+        csvdata<-read.csv(file)
+        data<-c(data,csvdata[pollutant][,1])
+    }
+
+    mean(data,na.rm=TRUE)
+}
